25cf74f37dcc5f6ee24b1dca099a46a0def696a6
Edi Modric <edi.modric@gmail.com>
2012-09-13 18:05:27 +0200

    Add scripts to test trash API

---
+++ trash/emptyTrash.sh
@@ -0,0 +1,3 @@
+http -v DELETE localhost:8042/content/trash \
+'X-Test-User:14' \
+'Accept:application/vnd.ez.api.Trash+xml'
---
+++ trash/removeTrashItem.sh
@@ -0,0 +1,3 @@
+http -v DELETE localhost:8042/content/trash/72 \
+'X-Test-User:14' \
+'Accept:application/vnd.ez.api.TrashItem+xml'
