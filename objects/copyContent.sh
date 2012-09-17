8b8bbd4ef84bdd56b1fbced6256bdcbc09e9da44
Edi Modric <edi.modric@gmail.com>
2012-09-17 16:25:45 +0200

    Add copy and delete object scripts

---
+++ objects/copyContent.sh
@@ -0,0 +1,4 @@
+http -v POST localhost:8042/content/objects/58 \
+'X-Test-User:14' \
+'Destination:/content/locations/1/5' \
+'X-HTTP-Method-Override:COPY'
---
+++ objects/deleteContent.sh
@@ -0,0 +1,3 @@
+http -v DELETE localhost:8042/content/objects/58 \
+'X-Test-User:14' \
+'Accept:application/vnd.ez.api.Content+xml'
