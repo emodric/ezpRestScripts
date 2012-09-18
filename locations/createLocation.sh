a03211d9b8b180fa0187b33b919442af3440467d
Edi Modric <edi.modric@gmail.com>
2012-09-18 18:59:01 +0200

    Update script for creating location

--- locations/createLocation.sh
+++ locations/createLocation.sh
@@ -1,8 +1,9 @@
 echo '<?xml version="1.0" encoding="UTF-8"?>
 <LocationCreate media-type="application/vnd.ez.api.LocationCreate+xml">
     <ParentLocation href="/content/locations/1/5" media-type="application/vnd.ez.api.Location+xml"/>
-    <priority>0</priority>
-    <hidden>false</hidden>
+    <priority>2</priority>
+    <hidden>true</hidden>
+    <remoteId>remoteId12345678</remoteId>
     <sortField>PATH</sortField>
     <sortOrder>ASC</sortOrder>
 </LocationCreate>' | \
