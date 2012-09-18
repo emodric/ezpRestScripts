7b5c20c7e3cf213f1a29fc495ebfaecb88741dce
Edi Modric <edi.modric@gmail.com>
2012-09-18 16:27:01 +0200

    Update scripts for copying current version

--- objects/createDraftFromCurrentVersion.sh
+++ objects/createDraftFromCurrentVersion.sh
@@ -1,4 +1,4 @@
-http -v POST localhost:8042/content/objects/58/versions \
+http -v POST localhost:8042/content/objects/58/currentversion \
 'X-Test-User:14' \
 'X-HTTP-Method-Override:COPY' \
 'Accept:application/vnd.ez.api.Version+xml'
