9a9929c45e41fdbfa7ffdf1956b1630aae65ab8d
Edi Modric <edi.modric@gmail.com>
2012-09-20 17:55:40 +0200

    Add scripts for loading users & user groups assigned to roles

---
+++ users/loadUserGroupsRefAssignedToRole.sh
@@ -0,0 +1,3 @@
+http -v localhost:8042/user/groups?roleId=/user/roles/2 \
+'X-Test-User:14' \
+'Accept:application/vnd.ez.api.UserGroupRefList+xml'
---
+++ users/loadUsersAssignedToRole.sh
@@ -0,0 +1,3 @@
+http -v localhost:8042/user/users?roleId=/user/roles/2 \
+'X-Test-User:14' \
+'Accept:application/vnd.ez.api.UserList+xml'
