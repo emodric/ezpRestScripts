b31c8f5554d6407b7885341bbf87689e1d8d5d1c
Edi Modric <edi.modric@gmail.com>
2012-09-21 17:14:33 +0200

    Fix accept header

--- xml/users/assignUserToUserGroup.sh
+++ xml/users/assignUserToUserGroup.sh
@@ -1,3 +1,3 @@
 http -v POST localhost:8042/user/users/14/groups?group=/1/5/44 \
 'X-Test-User:14' \
-'Accept:application/vnd.ez.api.UserGroupRefList+?group=/1/5/44'
+'Accept:application/vnd.ez.api.UserGroupRefList+xml'
