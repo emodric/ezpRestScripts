26d46f0d300404661be96eaeb561917ba39a91fe
Edi Modric <edi.modric@gmail.com>
2012-09-25 22:44:56 +0200

    Add scripts for creating & updating content type draft

---
+++ xml/content_types/createContentTypeDraft.sh
@@ -0,0 +1,24 @@
+echo '<?xml version="1.0" encoding="UTF-8"?>
+<ContentTypeUpdate>
+  <identifier>updatedContentType</identifier>
+  <mainLanguageCode>eng-GB</mainLanguageCode>
+  <remoteId>remoteId-qwert548</remoteId>
+  <urlAliasSchema>&lt;title&gt;</urlAliasSchema>
+  <nameSchema>&lt;title&gt;</nameSchema>
+  <isContainer>true</isContainer>
+  <defaultSortField>PATH</defaultSortField>
+  <defaultSortOrder>ASC</defaultSortOrder>
+  <defaultAlwaysAvailable>true</defaultAlwaysAvailable>
+  <names>
+    <value languageCode="eng-GB">Updated Content Type</value>
+  </names>
+  <descriptions>
+    <value languageCode="eng-GB">This is a description</value>
+  </descriptions>
+  <modificationDate>2012-12-31T12:30:00</modificationDate>
+  <User href="/user/users/14" />
+</ContentTypeUpdate>' | \
+http -v POST localhost:8042/content/types/1 \
+'X-Test-User:14' \
+'Accept:application/vnd.ez.api.ContentTypeInfo+xml' \
+'Content-Type:application/vnd.ez.api.ContentTypeUpdate+xml'
