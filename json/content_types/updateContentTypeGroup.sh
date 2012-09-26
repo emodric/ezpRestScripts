19b96a5351d8e303116405c40f579304c0b85bff
Edi Modric <edi.modric@gmail.com>
2012-09-26 18:11:45 +0200

    Update different group

--- json/content_types/updateContentTypeGroup.sh
+++ json/content_types/updateContentTypeGroup.sh
@@ -7,7 +7,7 @@ echo '{
     "modificationDate": "2012-12-31T12:00:00"
   }
 }' | \
-http -v POST localhost:8042/content/typegroups/5 \
+http -v POST localhost:8042/content/typegroups/1 \
 'X-Test-User:14' \
 'Accept:application/vnd.ez.api.ContentTypeGroup+json' \
 'Content-Type:application/vnd.ez.api.ContentTypeGroupInput+json' \
--- xml/content_types/updateContentTypeGroup.sh
+++ xml/content_types/updateContentTypeGroup.sh
@@ -4,7 +4,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
   <User href="/user/users/13" />
   <modificationDate>2012-12-31T12:00:00</modificationDate>
 </ContentTypeGroupInput>' | \
-http -v POST localhost:8042/content/typegroups/5 \
+http -v POST localhost:8042/content/typegroups/1 \
 'X-Test-User:14' \
 'Accept:application/vnd.ez.api.ContentTypeGroup+xml' \
 'Content-Type:application/vnd.ez.api.ContentTypeGroupInput+xml' \
