f432fc5ce805a73c24c6b0026b0f8357f4e396b3
Edi Modric <edi.modric@gmail.com>
2012-09-26 18:32:07 +0200

    Add scripts for /content/types

---
+++ json/content_types/loadContentTypeInfoByRemoteId.sh
@@ -0,0 +1,3 @@
+http -v localhost:8042/content/types?remoteId=a3d405b81be900468eb153d774f4f0d2 \
+'X-Test-User:14' \
+'Accept:application/vnd.ez.api.ContentTypeInfoList+json' \
---
+++ xml/content_types/loadContentTypeByIdentifier.sh
@@ -0,0 +1,3 @@
+http -v localhost:8042/content/types?identifier=folder \
+'X-Test-User:14' \
+'Accept:application/vnd.ez.api.ContentTypeList+xml' \
