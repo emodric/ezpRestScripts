c845a5b71b3f0f7032fb162a11c6e93d45baebe6
Edi Modrić <edi.modric@gmail.com>
2012-10-25 17:34:45 +0200

    Update some scripts for loading a location

--- xml/locations/loadLocationByRemote.sh
+++ xml/locations/loadLocationByRemote.sh
@@ -1,4 +1,3 @@
 source ../../rest.cfg
 
 http --auth $username:$password -v $host/api/ezp/v2/content/locations?remoteId=f3e90596361e31d496d4026eb624c983 \
-'Accept:application/vnd.ez.api.Location+xml'
