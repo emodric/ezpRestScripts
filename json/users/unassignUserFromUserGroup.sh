51f535226de247362abe9a0a86582655a6777f51
Edi Modrić <edi.modric@gmail.com>
2012-10-30 14:56:41 +0100

    Update user scripts

--- json/users/unassignUserFromUserGroup.sh
+++ json/users/unassignUserFromUserGroup.sh
@@ -1,4 +1,4 @@
 source ../../rest.cfg
 
-http --auth $username:$password -v DELETE $host/api/ezp/v2/user/users/10/groups/44 \
+http --auth $username:$password -v DELETE $host/api/ezp/v2/user/users/14/groups/44 \
 'Accept:application/vnd.ez.api.UserGroupRefList+json'
