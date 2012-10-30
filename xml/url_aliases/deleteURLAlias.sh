78c750dedd261b523925d86123b6cccc007b75f5
Edi Modrić <edi.modric@gmail.com>
2012-10-30 14:32:27 +0100

    Update URL alias scripts

--- xml/url_aliases/deleteURLAlias.sh
+++ xml/url_aliases/deleteURLAlias.sh
@@ -1,3 +1,3 @@
 source ../../rest.cfg
 
-http --auth $username:$password -v DELETE $host/api/ezp/v2/content/urlaliases/86-6f1ed002ab5595859014ebf0951522d9
+http --auth $username:$password -v DELETE $host/api/ezp/v2/content/urlaliases/85-d6fe1d0be6347b8ef2427fa629c04485
